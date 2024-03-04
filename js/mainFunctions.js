6dc15e86e32f66d66695a25e7397f8b31ce99f81
gtcore902 <gaetan.tremois@gmail.com>
2024-03-04 21:39:56 +0100

    style: change desgin & add BEM

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -9,8 +9,10 @@ let roverPerseverance = 'perseverance'; // curiosity
 let roverCuriosity = 'curiosity'; // curiosity
 let curiosityImg = document.getElementById('curiosityImg');
 let perseveranceImg = document.getElementById('perseveranceImg');
-let curiosityInfos = document.getElementById('dataImgCuriosity');
-let perseveranceInfos = document.getElementById('dataImgPerseverance');
+let curiosityInfos = document.querySelector('.content__information--curiosity');
+let perseveranceInfos = document.querySelector(
+  '.content__information--perseverance'
+);
 let lastPictureFromCuriosity = '';
 let lastPictureFromPerseverance = '';
 let lastDateCaptured = '';
@@ -48,7 +50,9 @@ const search = async (
       `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${lastDateCaptured}&api_key=bWdxMIS7efw8hwvaf40hjeezP3VBBairnjziMSMp`
     );
     const datas = await response.json();
-    imageContainer.setAttribute('src', `${datas.photos[0].img_src}`);
+    console.log(datas);
+    imageContainer.src = `${datas.photos[0].img_src}`;
+    imageContainer.alt = `Last picture from ${rover} on ${lastDateCaptured}`;
     information = `Rover ${datas.photos[0].rover.name} ${lastDateCaptured}`;
     textContainer.innerHTML = `${information} | `;
     let link = document.createElement('a');
@@ -58,7 +62,7 @@ const search = async (
     document.title = `Last pictures from Mars ${lastDateCaptured}`;
   } catch (error) {
     console.error(error);
-    document.getElementById('contentImg').innerHTML =
+    document.querySelector('.content').innerHTML =
       '<p>Sorry errors on server ... 😕<br /><br />Try again later !</p>';
   }
 };
