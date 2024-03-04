88d1a9a5b0bf112e93ba229a5d82d452609ba737
gtcore902 <gaetan.tremois@gmail.com>
2024-03-04 22:21:27 +0100

    refactor: remove comments

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,3 +1,8 @@
+/**
+ * Documentation at https://api.nasa.gov/ Mars Rover photos
+ */
+
+let API_KEY = 'bWdxMIS7efw8hwvaf40hjeezP3VBBairnjziMSMp';
 let information = '';
 let curiosityImg = document.querySelector('.content__image--curiosity');
 let perseveranceImg = document.querySelector('.content__image--perseverance');
@@ -9,20 +14,24 @@ let lastPictureFromCuriosity = '';
 let lastPictureFromPerseverance = '';
 let lastDateCaptured = '';
 
+/**
+ * Get the most recent Earth date from which photos exist
+ */
 const getMaxDate = async () => {
   try {
     const response = await fetch(
-      'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=0&api_key=bWdxMIS7efw8hwvaf40hjeezP3VBBairnjziMSMp'
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=0&api_key=${API_KEY}`
     );
     const datas = await response.json();
-    console.log(datas);
+    // console.log(datas);
     lastDateCaptured = datas.photos[0].rover.max_date;
-    search(lastDateCaptured, 'curiosity', curiosityImg, curiosityInfos);
+    search(lastDateCaptured, 'curiosity', curiosityImg, curiosityInfos, 'on');
     search(
       lastDateCaptured,
       'perseverance',
       perseveranceImg,
-      perseveranceInfos
+      perseveranceInfos,
+      'and'
     );
   } catch (error) {
     console.log(error);
@@ -31,18 +40,26 @@ const getMaxDate = async () => {
 
 getMaxDate();
 
+/**
+ * Search last pictures for the last date
+ * @param {*} lastDateCaptured
+ * @param {*} rover
+ * @param {*} imageContainer
+ * @param {*} textContainer
+ */
 const search = async (
   lastDateCaptured,
   rover,
   imageContainer,
-  textContainer
+  textContainer,
+  coordinator
 ) => {
   try {
     const response = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${lastDateCaptured}&api_key=bWdxMIS7efw8hwvaf40hjeezP3VBBairnjziMSMp`
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${lastDateCaptured}&api_key=${API_KEY}`
     );
     const datas = await response.json();
-    console.log(datas);
+    // console.log(datas);
     imageContainer.src = `${datas.photos[0].img_src}`;
     imageContainer.alt = `Last picture from ${rover} on ${lastDateCaptured}`;
     information = `Rover ${datas.photos[0].rover.name} ${lastDateCaptured}`;
@@ -53,7 +70,7 @@ const search = async (
     link.textContent = 'Download?';
     link.setAttribute('download', '');
     textContainer.appendChild(link);
-    document.title = `Last pictures from Mars ${lastDateCaptured}`;
+    document.title += ` ${coordinator} ${lastDateCaptured}`;
   } catch (error) {
     console.error(error);
     document.querySelector('.content').innerHTML =
